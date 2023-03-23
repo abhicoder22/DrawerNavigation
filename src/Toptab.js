9be53c39759b83297a4856b9ca23e5c01eb22c0d
abhicoder22 <abhishekpal.ap98@gmail.com>
2023-03-23 13:34:51 +0530

    TopTabAdded

---
+++ src/Toptab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
+const Tab = createMaterialTopTabNavigator();
+import Chats from './chats';
+import Status from './status';
+import Calls from './calls';
+export default function Home() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Chats" component={Chats} />
+      <Tab.Screen name="Status" component={Status} />
+      <Tab.Screen name="Calls" component={Calls} />
+    </Tab.Navigator>
+  );
+}
