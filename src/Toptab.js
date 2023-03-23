92e4c8ebdb869a4ab0ac69f709217ecc449e55f9
abhicoder22 <abhishekpal.ap98@gmail.com>
2023-03-23 16:23:30 +0530

    TopTabBackColor

--- src/Toptab.js
+++ src/Toptab.js
@@ -6,7 +6,11 @@ import Status from './status';
 import Calls from './calls';
 export default function Home() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        tabBarStyle: {backgroundColor: '#a83ea2'},
+        tabBarActiveTintColor: '#fff',
+      }}>
       <Tab.Screen name="Chats" component={Chats} />
       <Tab.Screen name="Status" component={Status} />
       <Tab.Screen name="Calls" component={Calls} />
